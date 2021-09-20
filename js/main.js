2dbba539788808beaa43e470efb36f1d784192d1
avaz-dev <avaz.dev@icloud.com>
2021-09-20 17:39:27 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var raqam = [1,22,3,4,55,88,7,];
+
+function numadd(num) {
+  var natija = 0;
+  for (var numadd of num) {
+    natija += numadd
+  }
+  return natija
+}
+console.log(numadd(raqam));
